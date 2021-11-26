e1e9c76780e6a54e58fedf53dea760c9f9444336
PraveenPra <prvp6501@gmail.com>
2021-11-26 22:49:49 +0530

    installed material ui, made header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,68 @@
 import React from 'react'
+import styled from 'styled-components'
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
-        <div>
-            header
-        </div>
+        <Container>
+          <a>
+              <img src='/images/logo.svg'/>
+                      </a>
+
+                      <Menu>
+                          <a href='#'>Model s</a>
+                      <a href='#'>Model s</a>
+                      <a href='#'>Model s</a>
+                      <a href='#'>Model s</a>
+
+                      </Menu>
+                      <RightMenu>
+                          <a href='#'>Shop</a>
+                          <a href='#'>Tesla Account</a>
+                          <CustomMenu/>
+
+                        
+                      </RightMenu>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+min-height:60px;
+position:fixed;
+display:flex;
+justify-content:space-between;
+align-items:center;
+padding:0 20px;
+top:0;
+left:0;
+right:0;
+`
+const Menu = styled.div`
+display:flex;
+align-items:center;
+a{color:black;
+    font-weight:600;
+    text-transform:uppercase;
+    padding:0 10px;
+    flex-wrap:nowrap;
+}
+
+@media(max-width:768px){
+    display:none;
+}
+`
+const RightMenu = styled.div`
+display:flex;
+align-items:center;
+a{color:black;
+    font-weight:600;
+    text-transform:uppercase;
+    margin-right:10px;
+}
+`
+const CustomMenu = styled(MenuIcon)`
+cursor:pointer;
+`
